ecd8539c20cd68536d23ac6a06e66028e54e9b2d
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-07-05 17:30:34 +0100

    Added tasks to html

--- script.js
+++ script.js
@@ -1,79 +1,91 @@
-
 // Task Class: Represents a Task
 class Task {
-    constructor(title) { 
-      this.title = title;
-    }
+  constructor(title) {
+    this.title = title;
+  }
 }
 
 // UI Class: Handle UI Tasks
-class UI{
 
-    static displayTasks(){}
+// Create a blank array of tasks
+const taskArray = [];
+class UI {
+  static displayTasks() {}
 
-    static addTaskToList(task) {}
+  static addTaskToList(task) {
+    // Take task list from html
+    const displayTask = document.querySelector("#task-list");
+    // Add paragraph elements as a child to the list
+    const para = document.createElement("p");
 
-    static deleteTask(element) {}
+    // Add each task submitted to the array
+    taskArray.push(task.title);
 
-    static clearFields() {
-        document.querySelector('#form-task-name').value = '';
-        // document.querySelector('#task-list').innerHTML = '';
-    }
+    // Loop through the array and for each task submitted create a new p element with the text as the task
+    taskArray.map((task) => {
+      para.innerText = task;
+    });
 
+    // Append each task to the task list container
+    displayTask.appendChild(para);
+  }
+
+  static deleteTask(element) {}
+
+  static clearFields() {
+    document.querySelector("#form-task-name").value = "";
+    // document.querySelector('#task-list').innerHTML = '';
+  }
 }
 
 // Store Class: Handles Storage
-class Store{
-
-    static getTasks(){
-        let tasks;
-        if(localStorage.getItem('tasks') === null) {
-          tasks = [];
-        } else {
-          tasks = JSON.parse(localStorage.getItem('tasks'));
-        }
-        return tasks;
+class Store {
+  static getTasks() {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
     }
+    return tasks;
+  }
 
-    static addTask(a_task){
-        const tasks = Store.getTasks();
-        tasks.push(a_task);
-        localStorage.setItem('tasks',JSON.stringify(tasks));
-    }
+  static addTask(a_task) {
+    const tasks = Store.getTasks();
+    tasks.push(a_task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-    static removeTask(){}
+  static removeTask() {}
 }
 
-
 // Event : Display Tasks
-document.addEventListener('DOMContentLoaded', UI.displayTasks);
+document.addEventListener("DOMContentLoaded", UI.displayTasks);
 
 // Event: Add a task
 document.querySelector(".form-task").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Get form values
-    const title = document.querySelector("#form-task-name").value;
+  // Get form values
+  const title = document.querySelector("#form-task-name").value;
 
-    const task = new Task(title);
+  const task = new Task(title);
 
-    // ToDo: Add task to UI
-    UI.addTaskToList(task);
+  // ToDo: Add task to UI
+  UI.addTaskToList(task);
 
-    // Add task to store
-    Store.addTask(task);
+  // Add task to store
+  Store.addTask(task);
 
-    // Clear fields
-    UI.clearFields();
-
+  // Clear fields
+  UI.clearFields();
 });
 
 // Event: Remove a task
-document.querySelector('#task-list').addEventListener('click', (e) => {
-    
-    // Remove task from UI
-    UI.deleteTask(e.target);
+document.querySelector("#task-list").addEventListener("click", (e) => {
+  // Remove task from UI
+  UI.deleteTask(e.target);
 
-    // Remove task from store
-    Store.removeTask();
+  // Remove task from store
+  Store.removeTask();
 });
